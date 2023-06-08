2c8ffc750c73a89ac283a0f8e3513153d90e55db
Bunyamin Syuaip <gundul@gmail.com>
2023-06-08 08:11:29 +0700

    Create 003BR-DB-Memory-Buffer-Pool.sql

---
+++ SCRIPTS/003BR-DB-Memory-Buffer-Pool.sql
@@ -0,0 +1,2 @@
+--bpool usage per db in kb:
+select db_name(database_id) as database_name, count(page_id)*8 as kb_usage from sys.dm_os_buffer_descriptors where database_id !=32767 group by database_id order by database_id
