49d99e01cdb9f9cb305fb523e4643e8477bf225f
Bunyamin Syuaip <gundul@gmail.com>
2023-09-01 06:48:54 +0700

    Add files via upload

---
+++ SCRIPTS/016C2_VLF_2012.sql
@@ -0,0 +1,32 @@
+Create Table #stage(
+    RecoveryUnitID int
+  , FileID      int
+  , FileSize    bigint
+  , StartOffset bigint
+  , FSeqNo      bigint
+  , [Status]    bigint
+  , Parity      bigint
+  , CreateLSN   numeric(38)
+);
+ 
+Create Table #results(
+    Database_Name   sysname
+  , VLF_count       int 
+);
+ 
+Exec sp_msforeachdb N'Use ?; 
+            Insert Into #stage 
+            Exec sp_executeSQL N''DBCC LogInfo(?)''; 
+ 
+            Insert Into #results 
+            Select DB_Name(), Count(*) 
+            From #stage; 
+ 
+            Truncate Table #stage;'
+ 
+Select * 
+From #results
+Order By VLF_count Desc;
+ 
+Drop Table #stage;
+Drop Table #results;
